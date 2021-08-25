a0d831a3c6da3389152f3e875cdccd7432130048
Kevin Catino <kcatino@itba.edu.ar>
2021-08-25 20:50:15 -0300

    test

--- src/guia2/EjA1.java
+++ src/guia2/EjA1.java
@@ -1,5 +1,6 @@
 package guia2;
 
+import org.apache.commons.math3.fraction.Fraction;
 import tools.VAD;
 
 public class EjA1 {
@@ -7,6 +8,8 @@ public class EjA1 {
         int[] range = {-10,10};
         double[] prob = {19.0/37,18.0/37};
         VAD vad = new VAD(range, prob);
-        System.out.println(vad);
+        Fraction f = new Fraction(0.5);
+        Fraction f2 = f.multiply(2);
+        System.out.println(f2);
     }
 }
